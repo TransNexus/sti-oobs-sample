1829b36eaff0847295439236f7ad11db995458c3
Alec Fenichel <alec.fenichel@transnexus.com>
2025-04-15 09:21:14 -0400

    Update node and packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,8 @@
-import globals from 'globals';
 import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
   {
-    languageOptions: {
-      globals: globals.node,
-    },
     files: [
       '*.mjs',
     ],
